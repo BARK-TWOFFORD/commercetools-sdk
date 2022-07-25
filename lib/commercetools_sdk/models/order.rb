cebe1eb5d71f8be67f1dcba2a7e5eb105fbae927
taylor-bark <twofford@barkbox.com>
2022-07-25 14:05:29 -0400

    Remove super calls in Order; Order has no parent

--- lib/commercetools_sdk/models/order.rb
+++ lib/commercetools_sdk/models/order.rb
@@ -214,9 +214,6 @@ module CommercetoolsSdk
         h[k.to_sym] = v
       }
 
-      # call parent's initialize
-      super(attributes)
-
       if attributes.key?(:'id')
         self.id = attributes[:'id']
       end
@@ -486,7 +483,6 @@ module CommercetoolsSdk
     # @return [Object] Returns the model itself
     def build_from_hash(attributes)
       return nil unless attributes.is_a?(Hash)
-      super(attributes)
       self.class.openapi_types.each_pair do |key, type|
         if type =~ /\AArray<(.*)>/i
           # check to ensure the input is an array given that the attribute
